5064383618f2cc8ca3a3d819c78a188cd958e5e0
lunita13 <irene_fedaoui@orange.fr>
2018-11-13 21:52:55 +0100

    suppression commentaires

--- model/UserManager.php
+++ model/UserManager.php
@@ -20,9 +20,7 @@ class UserManager
             $post = $req->fetch();
             
             $nombreEnreg=$post['nombre'];
-//            $Requete = mysqli_query($db, "SELECT * FROM users WHERE pseudo = '".$Pseudo."' AND mdp = '".$MotDePasse."'");
-//            $nombreEnreg=mysqli_num_rows($Requete);
-                //$loginCorrect = ($nombreEnreg>0);
+
             if($nombreEnreg==0) {
                 $LoginCorrect=false;
             } else {
@@ -33,30 +31,5 @@ class UserManager
     }
     
     
-//    public function VerificationOK($Pseudo, $MotDePasse)
-//    {
-//        $db = $this->dbConnect();
-//        
-//        if(!$db){
-//            echo "Erreur de connexion à la base de données.";
-//        }else {
-//            // on fait maintenant la requête dans la base de données pour rechercher si ces données existe et correspondent:
-//
-//            $Requete = mysqli_query($db, "SELECT * FROM users WHERE pseudo = '".$Pseudo."' AND mdp = '".$MotDePasse."'");
-//            $nombreEnreg=mysqli_num_rows($Requete);
-//                //$loginCorrect = ($nombreEnreg>0);
-//            if($nombreEnreg==0) {
-//                $LoginCorrect=false;
-//            } else {
-//                $LoginCorrect=true;
-//            }
-//            return $LoginCorrect;
-//        }
-//    }
-//    
-//    private function dbConnect()
-//    {
-//        $db = mysqli_connect('localhost', 'root', '', 'blog_auteur');
-//        return $db;
-//    }
+
 }
